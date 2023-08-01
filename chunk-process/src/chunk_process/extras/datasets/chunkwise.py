1e97e64a0fe2be2ec784b8a2c39d813e7c8988a7
Nok Chan <nok.lam.chan@quantumblack.com>
2023-08-01 19:44:44 +0100

    simplify chunk

--- chunk-process/src/chunk_process/extras/datasets/chunkwise.py
+++ chunk-process/src/chunk_process/extras/datasets/chunkwise.py
@@ -12,65 +12,12 @@ from kedro.io.core import (
     get_filepath_str,
     get_protocol_and_path,
 )
+from kedro.extras.datasets.pandas import CSVDataSet
 
-
-class ChunkWiseCSVDataSet(AbstractVersionedDataSet[pd.DataFrame, pd.DataFrame]):
+class ChunkWiseCSVDataSet(CSVDataSet):
     """``ChunkWiseCSVDataSet`` loads/saves data from/to a CSV file using an underlying
     filesystem. It uses pandas to handle the CSV file.
     """
-
-    DEFAULT_LOAD_ARGS = {}  # type: Dict[str, Any]
-    DEFAULT_SAVE_ARGS = {"index": False}  # type: Dict[str, Any]
-
-    def __init__(
-        self,
-        filepath: str,
-        load_args: Dict[str, Any] = None,
-        save_args: Dict[str, Any] = None,
-        version: Version = None,
-        credentials: Dict[str, Any] = None,
-        fs_args: Dict[str, Any] = None,
-    ) -> None:
-        """Creates a new instance of ``ChunkWiseCSVDataSet`` pointing to a concrete CSV file
-        on a specific filesystem.
-        """
-        _fs_args = deepcopy(fs_args) or {}
-        _credentials = deepcopy(credentials) or {}
-
-        protocol, path = get_protocol_and_path(filepath, version)
-        if protocol == "file":
-            _fs_args.setdefault("auto_mkdir", True)
-
-        self._protocol = protocol
-        self._storage_options = {**_credentials, **_fs_args}
-        self._fs = fsspec.filesystem(self._protocol, **self._storage_options)
-
-        super().__init__(
-            filepath=PurePosixPath(path),
-            version=version,
-            exists_function=self._fs.exists,
-            glob_function=self._fs.glob,
-        )
-
-        # Handle default load and save arguments
-        self._load_args = deepcopy(self.DEFAULT_LOAD_ARGS)
-        if load_args is not None:
-            self._load_args.update(load_args)
-        self._save_args = deepcopy(self.DEFAULT_SAVE_ARGS)
-        if save_args is not None:
-            self._save_args.update(save_args)
-
-    def _describe(self) -> Dict[str, Any]:
-        return {
-            "filepath": self._filepath,
-            "protocol": self._load_args,
-            "save_args": self._save_args,
-            "version": self._version,
-        }
-
-    def _load(self) -> pd.DataFrame:
-        load_path = str(self._get_load_path())
-        return pd.read_csv(load_path, **self._load_args)
 
     def _save(self, data: pd.DataFrame) -> None:
         save_path = get_filepath_str(self._get_save_path(), self._protocol)
@@ -78,5 +25,5 @@ class ChunkWiseCSVDataSet(AbstractVersionedDataSet[pd.DataFrame, pd.DataFrame]):
         buf = BytesIO()
         data.to_csv(path_or_buf=buf, **self._save_args)
 
-        with self._fs.open(save_path, mode="ab+") as fs_file:
+        with self._fs.open(save_path, mode="ab") as fs_file:
             fs_file.write(buf.getvalue())
--- chunk-process/src/chunk_process/nodes.py
+++ chunk-process/src/chunk_process/nodes.py
@@ -22,22 +22,13 @@ def split_data(
         Split data.
     """
     # Loop through data in chunks building up the training and test sets
-    for chunk in data:  # Iterate over the chunks from data
-        full_data = pd.concat(
-            [chunk]
-        )  # Converts the TextFileReader object into list of DataFrames
-        data_train = full_data.sample(
-            frac=parameters["train_fraction"], random_state=parameters["random_state"]
-        )
-        data_test = full_data.drop(data_train.index)
-
-        X_train = data_train.drop(columns=parameters["target_column"])
-        X_test = data_test.drop(columns=parameters["target_column"])
-        y_train = data_train[parameters["target_column"]]
-        y_test = data_test[parameters["target_column"]]
-        yield X_train
+    for i, chunk in enumerate(data):  # Iterate over the chunks from data
+        print(f"Process the {i} chunk of data")
+        yield chunk
 
 
 def process_chunk(chunk_data):
     for i, chunk in enumerate(chunk_data):
         print(i, chunk.head())
+
+    return "dummy"
