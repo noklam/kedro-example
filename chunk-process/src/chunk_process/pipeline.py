2044e52219f7d8a9c8d72d8306cb45603c0534c1
Nok Chan <nok.lam.chan@quantumblack.com>
2023-08-01 14:03:57 +0100

    simplify the example to use chunk processing

--- chunk-process/src/chunk_process/pipeline.py
+++ chunk-process/src/chunk_process/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.6
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import make_predictions, report_accuracy, split_data
+from .nodes import process_chunk, split_data
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -14,20 +14,14 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(
                 func=split_data,
                 inputs=["example_iris_data", "parameters"],
-                outputs=["X_train", "X_test", "y_train", "y_test"],
+                outputs="X_train_chunk",
                 name="split",
             ),
             node(
-                func=make_predictions,
-                inputs=["X_train", "X_test", "y_train"],
-                outputs="y_pred",
-                name="make_predictions",
-            ),
-            node(
-                func=report_accuracy,
-                inputs=["y_pred", "y_test"],
-                outputs=None,
-                name="report_accuracy",
+                func=process_chunk,
+                inputs="X_train_chunk",
+                outputs="dummy",
+                name="process_chunk",
             ),
         ]
     )
